3321f19705d28f34ad2ee8ca26976d00dbba9118
Adam Bright <abright@utilitywarehouse.co.uk>
2018-08-20 13:18:10 +0100

    updating now reinstalled

--- Tutorial2.go
+++ Tutorial2.go
@@ -2,13 +2,13 @@ package main
 
 import "fmt"
 
-func main() {
-   var a, b, c = 3, 4, "hello"  
-	
-   fmt.Println(a)
-   fmt.Println(b)
-   fmt.Println(c)
-   fmt.Printf("a is of type %T\n", a)
-   fmt.Printf("b is of type %T\n", b)
-   fmt.Printf("c is of type %T\n", c)
+func Tutorial2() {
+	var a, b, c = 3, 4, "hello"
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Printf("a is of type %T\n", a)
+	fmt.Printf("b is of type %T\n", b)
+	fmt.Printf("c is of type %T\n", c)
 }
